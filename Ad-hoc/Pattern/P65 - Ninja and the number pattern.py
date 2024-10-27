147d0845a7308488f6ea2a20254022999075089d
Nasiful Alam <nasifulalam1212@gmail.com>
2024-10-27 10:10:02 +0600

    Update P65 - Ninja and the number pattern.py

--- Ad-hoc/Pattern/P65 - Ninja and the number pattern.py
+++ Ad-hoc/Pattern/P65 - Ninja and the number pattern.py
@@ -1,3 +1,7 @@
+# This is a genius problem. To solve it, you need to think out of the box. 
+# Here, the numbers in each cell represent the minimum distance from each border to that cell. 
+# Once you understand this, the job is done.
+
 def getNumberPattern(n: int) -> None:
     for i in range(2*n-1):
         for j in range(2*n-1):
